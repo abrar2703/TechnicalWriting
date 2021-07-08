32559eeeb3b2c23c54c7f933f50db57a016203ec
Shaik Adil Ahmed <Shaik Adil Ahmed@LAPTOP-VV4AF63F>
2021-07-08 14:01:42 +0530

    Print

---
+++ Topics/out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,34 @@
+define(function() {var keywords=[{w:"Accountability",p:["p0"]},{w:"Batch",p:["p1","p24"]},{w:"Document",p:["p1"]},{w:"Cleaning",p:["p2","p3","p4","p5","p6"]},{w:"and",p:["p2","p3","p4","p7","p15"]},{w:"verification",p:["p2","p25"]},{w:"of",p:["p3","p4","p5","p6","p10","p15","p17","p24"]},{w:"Contact",p:["p3"]},{w:"Surfaces",p:["p3"]},{w:"Tools",p:["p3","p23"]},{w:"Pump",p:["p4","p6","p15"]},{w:"Trunions",p:["p4"]},{w:"all",p:["p4"]},{w:"Swagelok",p:["p4"]},{w:"fittings",p:["p4"]},{w:"Slot",p:["p5","p15"]},{w:"Die",p:["p5","p15"]},{w:"Zenith",p:["p6"]},{w:"Coater",p:["p7"]},{w:"Startup",p:["p7"]},{w:"Shutdown",p:["p7"]},{w:"Procedure",p:["p7","p16"]},{w:"Constraint",p:["p8"]},{w:"Contents",p:["p9"]},{w:"Dress",p:["p10"]},{w:"code",p:["p10"]},{w:"operators",p:["p10","p19"]},{w:"Introduction",p:["p11"]},{w:"Overview",p:["p12"]},{w:"Pre-Setup",p:["p13","p14"]},{w:"or",p:["p14"]},{w:"a",p:["p14"]},{w:"coating",p:["p14"]},{w:"run",p:["p14"]},{w:"Preparation",p:["p15"]},{w:"setup",p:["p15"]},{w:"Procurement",p:["p17"]},{w:"the",p:["p17"]},{w:"Resource",p:["p17"]},{w:"Release",p:["p18"]},{w:"Liners",p:["p18"]},{w:"Required",p:["p19"]},{w:"Responsibility",p:["p20"]},{w:"Scope",p:["p21"]},{w:"Standard",p:["p22"]},{w:"Operating",p:["p22"]},{w:"Procedure:",p:["p22"]},{w:"KK-1208-2",p:["p22"]},{w:"Validation",p:["p24"]},{w:"Documents",p:["p24"]},{w:"Purpose",p:["p26"]}];
+var ph={};
+ph["p0"]=[0];
+ph["p1"]=[1, 2];
+ph["p2"]=[3, 4, 5];
+ph["p3"]=[3, 6, 7, 8, 4, 9];
+ph["p4"]=[3, 6, 10, 11, 4, 12, 13, 14];
+ph["p5"]=[3, 6, 15, 16];
+ph["p6"]=[3, 6, 17, 10];
+ph["p7"]=[18, 19, 4, 20, 21];
+ph["p8"]=[22];
+ph["p9"]=[23];
+ph["p10"]=[24, 25, 6, 26];
+ph["p12"]=[28];
+ph["p11"]=[27];
+ph["p14"]=[29, 30, 31, 32, 33];
+ph["p13"]=[29];
+ph["p16"]=[21];
+ph["p15"]=[34, 6, 10, 4, 15, 16, 35];
+ph["p18"]=[39, 40];
+ph["p17"]=[36, 6, 37, 38];
+ph["p19"]=[41, 26];
+ph["p21"]=[43];
+ph["p20"]=[42];
+ph["p23"]=[9];
+ph["p22"]=[44, 45, 46, 47];
+ph["p25"]=[5];
+ph["p24"]=[48, 6, 1, 49];
+ph["p26"]=[50];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
